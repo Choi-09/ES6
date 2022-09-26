da3f1bc83abd3354f8ab05ca0cebec0326b5caea
vanessajchoi <vanessajchoi@gmail.com>
2022-09-26 17:50:21 +0900

    js 화살표함수. 반드시 외우기!

--- 01/scripts/index.js
+++ 01/scripts/index.js
@@ -1,9 +1,15 @@
 // 기존 함수 선언 방식
 /*function funPrint() {
+    console.log(n);
     console.log("외부 자바스크립트");
+    console.log("버튼"+1);
 } */
 
 //ES6+ 부터 : 화살표 함수 사용
-const funPrint = () => {
-    console.log("외부 자바스크립트");
+const funPrint = (n) => {
+    // console.log(n);
+    // console.log("외부 자바스크립트");
+    console.log(`버튼${n}`);
 }
+
+// `` : 백틱(1옆에)
