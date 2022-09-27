ec36e1d09d791f8bfbbc10addac78dbf40a00319
vanessajchoi <vanessajchoi@gmail.com>
2022-09-27 16:47:06 +0900

    변수, 함수 선언

--- 01/scripts/index.js
+++ 01/scripts/index.js
@@ -12,6 +12,11 @@ const funPrint = (n) => {
     console.log(`버튼${n}`);
 }
 
+// 변수
+// // console.log(x);
+// let x = 100;
+// x = 100*2;
+// console.log(x);
 
 /* jQuery
 $document.ready(){  
@@ -19,29 +24,42 @@ $document.ready(){
 */
 // document.addEventListener("DOMContentLoaded", 콜백함수)
 // document에 이벤트리스너 추가하는데 (DOM이 다 만들어지면, 익명함수 () =>{} 를 실행한다)
+
 document.addEventListener("DOMContentLoaded", () => {
 //console.log("DOM test")
 // DOM에 짜져있는 bt 을 (id)로 찾고 선택하여 -> (아래처럼) 요소를 생성 or 갖고와서 보여주기
     console.log(document.getElementById("bt").innerHTML);
 
     /*요소 생성*/
-   let bt = document.createElement("button")
-   document.getElementById("bt").append(bt);
+// //    let bt = document.createElement("button")
+// //    document.getElementById("bt").append(bt);
     
-    /*요소 찾기*/
-    // 결과: HTMLCollection
-    const bts1 = document.getElementsByClassName("bt1");
-    console.log(bts1);
-    const bts2 = document.getElementsByTagName("button");
-    console.log(bts2);
+//     /*요소 찾기*/
+//     // 결과: HTMLCollection
+//     const bts1 = document.getElementsByClassName("bt1");
+//     console.log(bts1);
     
-    // 결과: Node >> 처음 만나는 값을 1개만 가져온다
-    const bts3 = document.querySelector(".bt1");
-    console.log(bts3);
+//     // 결과: HTMLCollection
+//     const bts2 = document.getElementsByTagName("button");
+//     console.log(bts2);
+    
+//     // 결과: Node >> 처음 만나는 값을 1개만 가져온다
+//     const bts3 = document.querySelector(".bt1");
+//     console.log(bts3);
 
     // 결과: NodeList >> 값 전체를 가져온다
-    const bts4 = document.querySelectorAll('button');
+    const bts4 = document.querySelectorAll(".bt1");
     console.log(bts4);
+
+    /* 반복문-노가다(?)
+    console.log(bts4[0].innerHTML);
+    console.log(bts4[0].innerText);
+    console.log(bts4[1].innerContent);
+    */
+    // 결과 NodeList를 반복으로 가져오기
+    for(let i = 0; i <bts4.length ; i++) {
+        console.log(i);
+    }
 });
 // `` : 백틱(1옆에)
 
