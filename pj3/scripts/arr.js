91d901702a7411aa4d745a0aec0c5e148e97f095
vanessajchoi <vanessajchoi@gmail.com>
2022-09-30 17:41:26 +0900

    배열 셔플

--- pj3/scripts/arr.js
+++ pj3/scripts/arr.js
@@ -40,49 +40,74 @@
 // console.log(arr2);
 
 /* 배열 정렬 */
-let num = [1,3,5,2,4];
-num.sort() ;
-console.log(num);
+// let num = [1,3,5,2,4];
+// num.sort() ;
+// console.log(num);
  
+// /* filter : 주어진 함수의 조건을 만족하는 요소를 모아 새로운 배열로 반환*/
+// //3보다 큰수 집합 
+// let nf = num.filter((x) => x > 3);
+// console.log(nf);
+
+
+// /* 실습 : 합집합, 교집합, 차집합 */
+// let setA = [1,2,3,4,5];
+// let setB = [4,5,6,7,8];
+
+// // 얕은 복사 : 주소값 복사
+// let setC = setA ;
+// console.log(`setC => ${setC}`);
+// setA[0] = 100;
+// console.log(`setC => ${setC}`);
+
+// // 깊은 복사: 원본은 두고 복사본 생성
+// setA[0] = 1;
+// let setD = [...setA] ;
+// console.log(`setD => ${setD}`);
+// setA[0] = 100;
+// console.log(`setD => ${setD}`);
+
+// //합집합
+// let union =  [...setA, ...setB]
+// console.log(union);
+
+// //교집합
+// let intersection =  setA.filter((item) => setB.includes(item));
+// console.log(intersection);
+
+// //차집합
+// let difference =  setA.filter((item) => !setB.includes(item));
+// console.log(difference);
+
+let num = [1,3,5,2,4];
+num.sort ();
+console.log(num);
 
 /* 배열 섞기*/
- 
+// 
+const shuffleOld = (num) => {
 
-/* filter : 주어진 함수의 조건을 만족하는 요소를 모아 새로운 배열로 반환*/
-//3보다 큰수 집합 
-let nf = num.filter((x) => x > 3);
-console.log(nf);
+    for(let i =0 ; i < num.length ; i++) {
+        let idx1 = Math.floor(Math.random() * 5); //0~4까지 랜덤수 생성
+        let idx2 = Math.floor(Math.random() * 5); //0~4까지 랜덤수 생성
 
+        if (idx1 == idx2) continue;
 
-/* 실습 : 합집합, 교집합, 차집합 */
-let setA = [1,2,3,4,5];
-let setB = [4,5,6,7,8];
+        // idx1 위치의 값과 idx2 위치의 값을 치환
+        let temp = num[idx1];
+        num[idx1] = num[idx2];
+        num[idx2] = temp;
 
-// 얕은 복사 : 주소값 복사
-let setC = setA ;
-console.log(`setC => ${setC}`);
-setA[0] = 100;
-console.log(`setC => ${setC}`);
+        // console.log(`idx1 = ${idx1}, idx2=${idx2}`);
+        // console.log(num);
+    }
+    return num;
+}
+console.log(shuffleOld(num));
 
-// 깊은 복사: 원본은 두고 복사본 생성
-setA[0] = 1;
-let setD = [...setA] ;
-console.log(`setD => ${setD}`);
-setA[0] = 100;
-console.log(`setD => ${setD}`);
-
-//합집합
-let union =  [...setA, ...setB]
-console.log(union);
-
-//교집합
-let intersection =  
-console.log(intersection);
-
-//차집합
-let difference =  
-console.log(difference);
-
-
-
+// sort함수를 이용한 셔플
+const shuffle = (array) => {
+    return array.sort(() => Math.random() - 0.5) ;
+}
+console.log(shuffle(num));
 
