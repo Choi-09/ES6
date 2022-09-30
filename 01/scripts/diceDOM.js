fb047965303111ee6c9d7075862edb28006017f8
vanessajchoi <vanessajchoi@gmail.com>
2022-09-30 14:52:15 +0900

    pj2

--- 01/scripts/diceDOM.js
+++ 01/scripts/diceDOM.js
@@ -5,25 +5,31 @@ let randNum ;
 let userNum ;
 
 //화면 보기 함수
-const disDom = (diceSectionP, formSectionP, resultSectionP, bt1P, bt2P)
+const disDom = (diceSectionP, formSectionP, resultSectionP, bt1P, bt2P) => {
+    document.getElementById("diceSection").style.display = diceSectionP;
+    document.getElementById("formSection").style.display = formSectionP;
+    document.getElementById("resultSection").style.display = resultSectionP;
+    document.getElementById("bt1").style.display = bt1P;
+    document.getElementById("bt2").style.display = bt2P;
+}
+
 
 //주사위
 const showDice = () => {
+// 랜덤수 생성
     randNum = Math.floor(Math.random()*6)+1 ;
     console.log(`주사위 랜덤수 => ${randNum}`);
+
+// DOM 제어
+    dispDom("none", "block", "none", "block", "none", "block");
 }
 
 //확인
 const showCheck = () => {
-    console.log("확인");
+    userNum = form1.num.value;
 
 //DOM 제어
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("diceSection").style.display = "none";
-    document.getElementById("formSection").style.display = "none";
-    document.getElementById("resultSection").style.display = "none";
-    document.getElementsByClassName("bt1").style.display = "none";
-    document.getElementsByClassName("bt2").style.display = "none";
+    dispDom("blcok", "noen", "blcok", "noen", "blcok", "noen");
 
     let tag = `<img src = "./images/${i}.png">`
 
@@ -32,5 +38,4 @@ document.addEventListener("DOMContentLoaded", () => {
     else OX = "X";
 
     tag = `${tag}<img src = "./images/${OX}.png">`
-});
-}
+};
