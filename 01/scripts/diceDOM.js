c0b9c9d6633082d3b3bfc15b9518c4a83dc554be
vanessajchoi <vanessajchoi@gmail.com>
2022-10-04 15:33:35 +0900

    pj2

--- 01/scripts/diceDOM.js
+++ 01/scripts/diceDOM.js
@@ -5,14 +5,19 @@ let randNum ;
 let userNum ;
 
 //화면 보기 함수
-const disDom = (diceSectionP, formSectionP, resultSectionP, bt1P, bt2P) => {
+const dispDom = (diceSectionP, formSectionP, resultSectionP, bt1P, bt2P, bt3P) => {
     document.getElementById("diceSection").style.display = diceSectionP;
     document.getElementById("formSection").style.display = formSectionP;
     document.getElementById("resultSection").style.display = resultSectionP;
     document.getElementById("bt1").style.display = bt1P;
     document.getElementById("bt2").style.display = bt2P;
+    document.getElementById("bt3").style.display = bt3P;
 }
 
+//첫화면
+// const start = () => {
+//     dispDom("none", "block", "none", "none", "block", "none");
+// }
 
 //주사위
 const showDice = () => {
@@ -21,15 +26,15 @@ const showDice = () => {
     console.log(`주사위 랜덤수 => ${randNum}`);
 
 // DOM 제어
-    dispDom("none", "block", "none", "block", "none", "block");
+    dispDom("block", "none", "none", "block", "none", "block");
 }
 
 //확인
-const showCheck = () => {
+const showResult = () => {
     userNum = form1.num.value;
 
 //DOM 제어
-    dispDom("blcok", "noen", "blcok", "noen", "blcok", "noen");
+    dispDom("blcok", "none", "blcok", "none", "blcok", "none");
 
     let tag = `<img src = "./images/${i}.png">`
 
@@ -38,4 +43,13 @@ const showCheck = () => {
     else OX = "X";
 
     tag = `${tag}<img src = "./images/${OX}.png">`
+
+    document.getElementById("diceImg").innerHTML = tag;
+    console.log(`사용자 선택 수 확인 => ${userNum}`);
 };
+
+//DOM이 생성이 되고 난 후에 요소를 CRUD해야한다.
+document.addEventListener("DOMContentLoaded", () => {
+    //주사위 버튼만 보여야 함
+    dispDom("none", "none", "block", "none"); 
+  }); 
