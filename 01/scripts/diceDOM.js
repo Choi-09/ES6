2def5f89db9f8f621757e46ff755d84721b7fb92
vanessajchoi <vanessajchoi@gmail.net>
2022-10-05 00:25:15 +0900

    edit img

--- 01/scripts/diceDOM.js
+++ 01/scripts/diceDOM.js
@@ -4,19 +4,15 @@ let randNum ;
 //사용자가 선택한 수
 let userNum ;
 
+
 //화면 보기 함수
-const dispDom = (diceSectionP, formSectionP, resultSectionP, bt1P, bt2P, bt3P) => {
+const dispDom = (diceSectionP, formSectionP, resultSectionP, bt2P, bt3P) => {
     document.getElementById("diceSection").style.display = diceSectionP;
     document.getElementById("formSection").style.display = formSectionP;
-    document.getElementById("resultSection").style.display = resultSectionP;
-    document.getElementById("bt1").style.display = bt1P;
+    // document.getElementById("resultSection").style.display = resultSectionP;
+    // document.getElementById("bt1").style.display = bt1P;
     document.getElementById("bt2").style.display = bt2P;
     document.getElementById("bt3").style.display = bt3P;
-}
-
-// 첫화면
-const start = () => {
-    dispDom("none", "block", "none", "none", "block", "none");
 }
 
 //주사위
@@ -26,7 +22,7 @@ const showDice = () => {
     console.log(`주사위 랜덤수 => ${randNum}`);
 
 // DOM 제어
-    dispDom("block", "none", "none", "block", "none", "block");
+    dispDom("none", "block", "none", "none", "block");
 }
 
 //확인
@@ -34,22 +30,23 @@ const showResult = () => {
     userNum = form1.num.value;
 
 //DOM 제어
-    dispDom("blcok", "none", "blcok", "none", "blcok", "none");
+    dispDom("blcok", "none", "blcok", "block", "none");
 
-    let tag = `<img src = "./images/${randNum}.png">`
+//주사위 그림
+    let tag = `<img src="./images/${randNum}.png">`;
 
-    let OX;
-    if (randNum ==userNum) OX = "O";
-    else OX = "X";
+//ox그림
+    let ox;
+    if (randNum ==userNum) ox = "o";
+    else ox = "x";
 
-    tag = `${tag}<img src = "./images/${OX}.png">`
-
-    document.getElementById("diceImg").innerHTML = tag;
+    tag = `${tag}<img src="./images/${ox}.png">`;
+    document.getElementsbyId("diceSection").innerHTML = tag;
     console.log(`사용자 선택 수 확인 => ${userNum}`);
-};
+}
 
 //DOM이 생성이 되고 난 후에 요소를 CRUD해야한다.
 document.addEventListener("DOMContentLoaded", () => {
-    //주사위 버튼만 보여야 함
-    dispDom("block", "none", "block", "none"); 
+    //주사위 던지기 버튼만 보여야 함
+    dispDom("none", "none", "none", "block", "none"); 
   }); 
